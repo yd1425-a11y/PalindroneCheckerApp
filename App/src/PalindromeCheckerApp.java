f4864cbc5383046427009bd6082e1423c3259853
yd1425-a11y <yd1425@srmist.edu.in>
2026-02-26 16:08:12 +0530

    Yash Dingar Added Usecase8

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,6 +1,41 @@
+class Node {
+    char data;
+    Node next;
+
+    Node(char d) {
+        data = d;
+    }
+}
+
 public class PalindromeCheckerApp {
-    public static void main(String[] args){
-        System.out.println("Hello World");
-        System.out.println("this is use case 2");
+    public static void main(String[] args) {
+        String str = "madam";
+        Node head = null, temp = null;
+
+        // Create linked list
+        for (char c : str.toCharArray()) {
+            Node newNode = new Node(c);
+            if (head == null) {
+                head = newNode;
+                temp = newNode;
+            } else {
+                temp.next = newNode;
+                temp = newNode;
+            }
+        }
+
+        // Check palindrome
+        String original = "", reverse = "";
+        temp = head;
+        while (temp != null) {
+            original += temp.data;
+            reverse = temp.data + reverse;
+            temp = temp.next;
+        }
+
+        if (original.equals(reverse))
+            System.out.println("Palindrome");
+        else
+            System.out.println("Not Palindrome");
     }
 }
