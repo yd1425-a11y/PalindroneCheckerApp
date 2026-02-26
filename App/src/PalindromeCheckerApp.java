9c71f4ec5bafa9b73e8b07cbdba32daa4c374017
yd1425-a11y <yd1425@srmist.edu.in>
2026-02-26 14:16:04 +0530

    Yash Dingar Added Usecase2

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,5 +1,6 @@
 public class PalindromeCheckerApp {
     public static void main(String[] args){
         System.out.println("Hello World");
+        System.out.println("this is use case 2");
     }
 }
